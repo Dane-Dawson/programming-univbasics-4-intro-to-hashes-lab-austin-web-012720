74730474164a57158fc35988d970d8ed4af1935b
Dane P Dawson <dane.p.dawson@gmail.com>
2020-01-13 20:51:40 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -19,14 +19,21 @@ def pioneer
 end
 
 def id_generator
-  id_hash
+  id_hash = {
+    id: "7"
+  }
+  return id_hash
 end
 
 def my_hash_creator(key, value)
-  # return a hash that includes the key and value parameters passed into this method
+  creator_hash = {
+    key: value
+  }
+  return creator_hash
 end
 
 def read_from_hash(hash, key)
+  return hash[key]
   # return the correct value using the hash and key parameters
 end
 
